fa3251429c7e14cae01c8ec56b4662e6de345b3b
Edgar3101 <edgar.hernandez3101@gmail.com>
2022-03-03 19:52:24 -0500

    Changes for production

---
+++ src/Products/BlockScreen.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+function BlockScreen(props){
+    const [see, setSee] = React.useState(true);
+
+    React.useEffect(() => {
+        
+    }, [])
+    return (
+        <div>
+
+
+        </div>
+        
+    )
+}
