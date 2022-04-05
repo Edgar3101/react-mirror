19756fbf9e26914950796e2395b975de987066a5
Edgar3101 <edgar.hernandez3101@gmail.com>
2022-04-05 18:57:06 -0500

    Fix error in sizes

--- src/Products/Fetch_Product.js
+++ src/Products/Fetch_Product.js
@@ -3,11 +3,14 @@
 export default async function FetchBuy(currentProduct, local){
     var size= local.getItem("size");
     var id= local.getItem("id");
+    local.removeItem('size');  
+    local.removeItem("id");  
     if(size !== null && id !== null){
         var websocket = new WebSocket("ws://localhost:8002/");
         websocket.onopen = () =>{
             console.log("Connected")
-            websocket.send(JSON.stringify({ 'message': {currentProduct, size, id}}))           
+            websocket.send(JSON.stringify({ 'message': {currentProduct, size, id}}))   
+                
         }
         
         
